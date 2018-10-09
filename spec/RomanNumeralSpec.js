53e6cefab0df48af64d61100431e066d5d1d7e6f
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-09 18:24:46 +0100

    Adds beforeEach function to create instance of Roman Numeral in spec file

--- spec/RomanNumeralSpec.js
+++ spec/RomanNumeralSpec.js
@@ -1,13 +1,18 @@
 describe("RomanNumeral", function() {
 
+    beforeEach(function() {
+        romanNumeral = new RomanNumeral();
+    })
+
     describe("A function to convert an integer to a roman numeral", function() {
         it("should return the Roman Numeral for 1", function() {
-            romanNumeral = new RomanNumeral();
             expect(romanNumeral.convertInteger(1)).toEqual("I");
         });
         it("should return the Roman Numeral for 5", function() {
-            romanNumeral = new RomanNumeral();
             expect(romanNumeral.convertInteger(5)).toEqual("V");
         });
+        it("should return the Roman Numeral for 10", function() {
+
+        })
     });
 })
