e3d17a38d2a4b06ef31cc2a2bcdcd35a34297c82
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-09 13:42:18 +0100

    rephrase testing

--- spec/RomanNumeralSpec.js
+++ spec/RomanNumeralSpec.js
@@ -1,6 +1,6 @@
 describe("RomanNumeral", function() {
 
-    describe("convert an integer to a roman numeral", function() {
+    describe("A function to convert an integer to a roman numeral", function() {
         it("should return the Roman Numeral for 1", function() {
             romanNumeral = new RomanNumeral();
             expect(romanNumeral.convertInteger(1)).toEqual("I");
