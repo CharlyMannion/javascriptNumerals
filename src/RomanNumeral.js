bd7e0065d1ea5944c16c4f867f99164e3069dcd1
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-11 10:40:24 +0100

    refactors to delete hashed out if else statement

--- src/RomanNumeral.js
+++ src/RomanNumeral.js
@@ -1,19 +1,3 @@
 function RomanNumeral() {
     this.basicNumerals = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 1000: "M"}
 }
-
-// RomanNumeral.prototype.convertInteger = function(integer) {
-//     if (integer === 1) {
-//         return 'I'
-//     } else if (integer === 5) {
-//         return 'V'
-//     } else if (integer === 10) {
-//         return 'X'
-//     } else if (integer === 50) {
-//         return 'L'
-//     } else if (integer === 100) {
-//         return 'C'
-//     } else if (integer === 1000) {
-//         return 'M'
-//     }
-// }
