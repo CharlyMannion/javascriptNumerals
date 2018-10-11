09428fec97b116e0c52b83d2bced32b02b887e17
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-11 10:45:12 +0100

    restores convert integer function

--- src/RomanNumeral.js
+++ src/RomanNumeral.js
@@ -1,3 +1,7 @@
 function RomanNumeral() {
     this.basicNumerals = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 1000: "M"}
 }
+
+RomanNumeral.prototype.convertInteger = function(integer) {
+    return integer;
+}
