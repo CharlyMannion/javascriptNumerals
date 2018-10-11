b954a8c3040dce2dad584e659d30ed86d5c03142
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-11 10:48:38 +0100

    tests the full range of basic numerals can be accessed with convert function

--- spec/RomanNumeralSpec.js
+++ spec/RomanNumeralSpec.js
@@ -35,20 +35,20 @@ describe("RomanNumeral", function() {
         it("should return the Roman Numeral for 1", function() {
             expect(romanNumeral.convertInteger(1)).toEqual("I");
         });
-        // it("should return the Roman Numeral for 5", function() {
-        //     expect(romanNumeral.convertInteger(5)).toEqual("V");
-        // });
-        // it("should return the Roman Numeral for 10", function() {
-        //     expect(romanNumeral.convertInteger(10)).toEqual("X");
-        // });
-        // it("should return the Roman Numeral for 50", function() {
-        //     expect(romanNumeral.convertInteger(50)).toEqual("L");
-        // });
-        // it("should return the Roman Numeral for 100", function() {
-        //     expect(romanNumeral.convertInteger(100)).toEqual("C");
-        // });
-        // it("should return the Roman Numeral for 1000", function() {
-        //     expect(romanNumeral.convertInteger(1000)).toEqual("M");
-        // });
+        it("should return the Roman Numeral for 5", function() {
+            expect(romanNumeral.convertInteger(5)).toEqual("V");
+        });
+        it("should return the Roman Numeral for 10", function() {
+            expect(romanNumeral.convertInteger(10)).toEqual("X");
+        });
+        it("should return the Roman Numeral for 50", function() {
+            expect(romanNumeral.convertInteger(50)).toEqual("L");
+        });
+        it("should return the Roman Numeral for 100", function() {
+            expect(romanNumeral.convertInteger(100)).toEqual("C");
+        });
+        it("should return the Roman Numeral for 1000", function() {
+            expect(romanNumeral.convertInteger(1000)).toEqual("M");
+        });
     });
 })
