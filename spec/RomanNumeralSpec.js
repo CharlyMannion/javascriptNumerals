725acd880653d2865dff4fd5605986d00bc39e6c
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-11 11:53:31 +0100

    tests converting 2 to a numeral

--- spec/RomanNumeralSpec.js
+++ spec/RomanNumeralSpec.js
@@ -31,24 +31,30 @@ describe("RomanNumeral", function() {
         })
     })
 
-    describe("A function to convert an integer to a roman numeral", function() {
+    describe("A function to convert a basic integer to a roman numeral", function() {
         it("should return the Roman Numeral for 1", function() {
             expect(romanNumeral.convertInteger(1)).toEqual("I");
-        });
+        })
         it("should return the Roman Numeral for 5", function() {
             expect(romanNumeral.convertInteger(5)).toEqual("V");
-        });
+        })
         it("should return the Roman Numeral for 10", function() {
             expect(romanNumeral.convertInteger(10)).toEqual("X");
-        });
+        })
         it("should return the Roman Numeral for 50", function() {
             expect(romanNumeral.convertInteger(50)).toEqual("L");
-        });
+        })
         it("should return the Roman Numeral for 100", function() {
             expect(romanNumeral.convertInteger(100)).toEqual("C");
-        });
+        })
         it("should return the Roman Numeral for 1000", function() {
             expect(romanNumeral.convertInteger(1000)).toEqual("M");
-        });
-    });
+        })
+    })
+
+    describe("A function to convert a more complicated number to a roman numeral", function() {
+        it("should return the Roman Numeral for 2", function() {
+            expect(romanNumeral.convertInteger(2)).toEqual("II");
+        })
+    })
 })
--- src/RomanNumeral.js
+++ src/RomanNumeral.js
@@ -2,6 +2,6 @@ function RomanNumeral() {
     this.basicNumerals = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 1000: "M"}
 }
 
-RomanNumeral.prototype.convertInteger = function(integer) {
-    return this.basicNumerals[integer];
+RomanNumeral.prototype.convertInteger = function(i) {
+    return (i === 1 || i === 5 || i === 10 || i === 50 || i === 100 || i === 1000) ? this.basicNumerals[i] : "II"
 }
