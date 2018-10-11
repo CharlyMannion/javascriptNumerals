c5f73eb8f5b68e3ca728e394fdf725815f55cfb5
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-11 10:24:14 +0100

    refactors to create a basic numerals variable

--- spec/RomanNumeralSpec.js
+++ spec/RomanNumeralSpec.js
@@ -4,24 +4,30 @@ describe("RomanNumeral", function() {
         romanNumeral = new RomanNumeral();
     })
 
-    describe("A function to convert an integer to a roman numeral", function() {
-        it("should return the Roman Numeral for 1", function() {
-            expect(romanNumeral.convertInteger(1)).toEqual("I");
+    describe("when you create a new roman numeral instance", function() {
+        it("should have an object containing the basic roman numerals", function() {
+            expect(romanNumeral.basicNumerals).toEqual("insert numerals object");
         });
-        it("should return the Roman Numeral for 5", function() {
-            expect(romanNumeral.convertInteger(5)).toEqual("V");
-        });
-        it("should return the Roman Numeral for 10", function() {
-            expect(romanNumeral.convertInteger(10)).toEqual("X");
-        });
-        it("should return the Roman Numeral for 50", function() {
-            expect(romanNumeral.convertInteger(50)).toEqual("L");
-        });
-        it("should return the Roman Numeral for 100", function() {
-            expect(romanNumeral.convertInteger(100)).toEqual("C");
-        });
-        it("should return the Roman Numeral for 1000", function() {
-            expect(romanNumeral.convertInteger(1000)).toEqual("M");
-        });
-    });
+    })
+
+    // describe("A function to convert an integer to a roman numeral", function() {
+    //     it("should return the Roman Numeral for 1", function() {
+    //         expect(romanNumeral.convertInteger(1)).toEqual("I");
+    //     });
+    //     it("should return the Roman Numeral for 5", function() {
+    //         expect(romanNumeral.convertInteger(5)).toEqual("V");
+    //     });
+    //     it("should return the Roman Numeral for 10", function() {
+    //         expect(romanNumeral.convertInteger(10)).toEqual("X");
+    //     });
+    //     it("should return the Roman Numeral for 50", function() {
+    //         expect(romanNumeral.convertInteger(50)).toEqual("L");
+    //     });
+    //     it("should return the Roman Numeral for 100", function() {
+    //         expect(romanNumeral.convertInteger(100)).toEqual("C");
+    //     });
+    //     it("should return the Roman Numeral for 1000", function() {
+    //         expect(romanNumeral.convertInteger(1000)).toEqual("M");
+    //     });
+    // });
 })
--- src/RomanNumeral.js
+++ src/RomanNumeral.js
@@ -1,18 +1,19 @@
 function RomanNumeral() {
+    this.basicNumerals = "insert numerals object"
 }
 
-RomanNumeral.prototype.convertInteger = function(integer) {
-    if (integer === 1) {
-        return 'I'
-    } else if (integer === 5) {
-        return 'V'
-    } else if (integer === 10) {
-        return 'X'
-    } else if (integer === 50) {
-        return 'L'
-    } else if (integer === 100) {
-        return 'C'
-    } else if (integer === 1000) {
-        return 'M'
-    }
-}
+// RomanNumeral.prototype.convertInteger = function(integer) {
+//     if (integer === 1) {
+//         return 'I'
+//     } else if (integer === 5) {
+//         return 'V'
+//     } else if (integer === 10) {
+//         return 'X'
+//     } else if (integer === 50) {
+//         return 'L'
+//     } else if (integer === 100) {
+//         return 'C'
+//     } else if (integer === 1000) {
+//         return 'M'
+//     }
+// }
