761793ca823fa90df07c3898827a3203ca2948a2
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-12 11:34:34 +0100

    create an array of the integers corresponding to the basic numerals

--- spec/RomanSpec.js
+++ spec/RomanSpec.js
@@ -8,5 +8,8 @@ describe("Roman", function() {
         it("should have an array of the basic roman numerals", function() {
             expect(roman.romanNumerals).toEqual(["M", "D", "C", "L", "X", "V,", "I"]);
         })
+        it("should have an array of the corresponding integers", function() {
+            expect(roman.basicIntegers).toEqual([1000, 500, 100, 50, 10, 1]);
+        })
     })
 })
--- src/Roman.js
+++ src/Roman.js
@@ -1,3 +1,4 @@
 function Roman() {
 this.romanNumerals = ["M", "D", "C", "L", "X", "V,", "I"]
+this.basicIntegers = [1000, 500, 100, 50, 10, 1]
 }
