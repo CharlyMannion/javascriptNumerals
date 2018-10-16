4030e09ff2c197cdc48cc0d17745a44781f124c9
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-16 11:13:25 +0100

    fixes spelling error

--- spec/RomanSpec.js
+++ spec/RomanSpec.js
@@ -6,7 +6,7 @@ describe("Roman", function() {
 
     describe("a new roman numeral instance", function() {
         it("should have an array of the basic roman numerals", function() {
-            expect(roman.romanNumerals).toEqual(["M", "D", "C", "L", "X", "V,", "I"]);
+            expect(roman.romanNumerals).toEqual(["M", "D", "C", "L", "X", "V", "I"]);
         })
         it("should have an array of the corresponding integers", function() {
             expect(roman.basicIntegers).toEqual([1000, 500, 100, 50, 10, 1]);
