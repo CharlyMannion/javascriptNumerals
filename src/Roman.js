39167e1cd1bc460db4b8000c7944d18143ebef01
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-17 10:08:29 +0100

    reverses terner

--- src/Roman.js
+++ src/Roman.js
@@ -7,9 +7,9 @@ this.convertedInteger = ""
 
 Roman.prototype.romanize = function(i) {
     for(var index = 0; index < this.basicIntegers.length; index++) {
-        // console.log(this.basicIntegers[index]);
-        // console.log(this.romanNumerals[index]);
-        return (this.basicIntegers[index] == i) ? this.romanNumerals[index] : "other";
+        console.log(this.basicIntegers[index]);
+        console.log(this.romanNumerals[index]);
+        return (i === this.basicIntegers[index]) ? this.romanNumerals[index] : "other";
     }
         // return (this.basicIntegers.includes(i)) ? "I" : "II";
     // LOOP THROUGH THE BASICINTEGERS ARRAY AND SEE IF I MATCHES THE VALUE. IF IT DOES, SAVE THE INDEX VALUE IN A VARIABLE. THEN USE THE VARIABLE TO LOOK UP THE VALUE AT THIS INDEX IN THE ROMANNUMERALS ARRAY. RETURN IT.
