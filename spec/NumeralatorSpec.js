6e6d62705847030514fa99087a02d8db5b2cc423
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-18 14:00:10 +0100

    writes test for integer 2

--- spec/NumeralatorSpec.js
+++ spec/NumeralatorSpec.js
@@ -8,9 +8,9 @@ describe("Numeralator", function() {
         it("should convert 1 to I", function() {
             expect(numeralator.convertInteger(1)).toEqual("I");
         })
-        // it("should convert 5 to V", function() {
-        //     expect(numeralator.convertInteger(5)).toEqual("V");
-        // })
+        it("should convert 2 to II", function() {
+            expect(numeralator.convertInteger(2)).toEqual("II");
+        })
     })
 
 })
