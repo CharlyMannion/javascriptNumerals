cc874b3f176017d43320b2f168972a7446b6b1ad
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-18 13:58:36 +0100

    creates numeralTypes object and still passes first test

--- src/Numeralator.js
+++ src/Numeralator.js
@@ -1,7 +1,8 @@
 function Numeralator() {
-
+    this.numeralTypes = [ { roman: "I", int: 1 }, { roman: "II", int: 2 }, { roman: "III", int: 3 }, { roman: "IV", int: 4 }, { roman: "V", int: 5 }]
 }
 
 Numeralator.prototype.convertInteger = function() {
-    return "I"
+    console.log(this.numeralTypes[0].roman);
+    return this.numeralTypes[0].roman;
 }
