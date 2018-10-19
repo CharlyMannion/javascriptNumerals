fe33d09baf7127df3b47629b45070cbc3aa44fa1
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-19 10:46:38 +0100

    convertInteger function converts 1 and 2 to numerals

--- src/Numeralator.js
+++ src/Numeralator.js
@@ -2,7 +2,7 @@ function Numeralator() {
     this.numeralTypes = [ { roman: "I", int: 1 }, { roman: "II", int: 2 }, { roman: "III", int: 3 }, { roman: "IV", int: 4 }, { roman: "V", int: 5 }]
 }
 
-Numeralator.prototype.convertInteger = function() {
-    console.log(this.numeralTypes[0].roman);
-    return this.numeralTypes[0].roman;
+Numeralator.prototype.convertInteger = function(num) {
+    // console.log(this.numeralTypes[0].roman);
+    return (this.numeralTypes[0].int === num) ? this.numeralTypes[0].roman : this.numeralTypes[1].roman;
 }
