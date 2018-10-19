d5c7bb6e307140876c9f1aec292ed175b3f2a9a1
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-19 10:50:20 +0100

    refactors tests and writes test for integer 3

--- spec/NumeralatorSpec.js
+++ spec/NumeralatorSpec.js
@@ -4,12 +4,15 @@ describe("Numeralator", function() {
         numeralator = new Numeralator();
     })
 
-    describe("A function to convert integers to roman numerals", function() {
+    describe("A function to convert integers 1-10 to roman numerals", function() {
         it("should convert 1 to I", function() {
             expect(numeralator.convertInteger(1)).toEqual("I");
         })
         it("should convert 2 to II", function() {
             expect(numeralator.convertInteger(2)).toEqual("II");
+        })
+        it("should convert 3 to III", function() {
+            expect(numeralator.convertInteger(3)).toEqual("III");
         })
     })
 
