7b319922c3dc95c95c2ffcca0829f3c181c76bcd
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-28 19:08:10 +0000

    writes case for integer 11 following revised logic

--- src/Numeralator.js
+++ src/Numeralator.js
@@ -3,10 +3,15 @@ function Numeralator() {
     var basicNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     this.rules = [ 
+        // {
+        //     matchRoman: function(integer) { if (integer % 10 === 5) return true},
+        //     getValue: function(integer) { return (integer === 15) ? "XV" :  "V" }
+        // },
         {
-            matchRoman: function(integer) { if (integer % 10 === 5) return true},
-            getValue: function(integer) { return (integer === 15) ? "XV" :  "V" }
+            matchRoman: function(integer) { if ( integer > 10 && integer <= 20 ) return true }, 
+            getValue: function(integer) { return "X" + "I" }
         },
+
         {
             matchRoman: function(integer) { if (basicNum.includes(integer)) return true }, 
             getValue: function(integer) { 
@@ -25,18 +30,19 @@ function Numeralator() {
                 return getNumeral(integer) 
             }
         },
-        {
-            matchRoman: function(integer) { if (integer % 10 === 1) return true},
-            getValue: function(integer) {
-                const getNumeral = (integer) => ({
-                    11: "XI",
-                    21: "XXI",
-                    31: "XXXI",
-                    41: "XXXXI"
-                })[integer];
-                return getNumeral(integer)   
-            } 
-        },
+
+        // {
+        //     matchRoman: function(integer) { if (integer % 10 === 1) return true},
+        //     getValue: function(integer) {
+        //         const getNumeral = (integer) => ({
+        //             11: "XI",
+        //             21: "XXI",
+        //             31: "XXXI",
+        //             41: "XXXXI"
+        //         })[integer];
+        //         return getNumeral(integer)   
+        //     } 
+        // },
     ]
 }
 
