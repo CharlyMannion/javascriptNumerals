0bf478e7cf113fdfd6a4aef69360f8386e2fcadc
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-29 10:01:48 +0000

    refactors rules to remove fixed values and replace with calls to numeral object

--- src/Numeralator.js
+++ src/Numeralator.js
@@ -13,29 +13,34 @@ function Numeralator() {
         7: "VII",
         8: "VIII",
         9: "IX",
-        10: "X"
+        10: "X",
+        20: "XX",
+        30: "XXX",
+        40: "XL",
+        50: "L",
+        60: "LX"
     })[integer];
 
     this.rules = [ 
         {
             matchRoman: function(integer) { if ( integer >= 60 && integer <= 69 ) return true }, 
-            getValue: function(integer) { return "LX" + getNumeral(integer-60) }
+            getValue: function(integer) { return getNumeral(60) + getNumeral(integer-60) }
         },
         {
             matchRoman: function(integer) { if ( integer >= 50 && integer <= 59 ) return true }, 
-            getValue: function(integer) { return "L" + getNumeral(integer-50) }
+            getValue: function(integer) { return getNumeral(50) + getNumeral(integer-50) }
         },
         {
             matchRoman: function(integer) { if ( integer >= 40 && integer <= 49 ) return true }, 
-            getValue: function(integer) { return "XL" + getNumeral(integer-40) }
+            getValue: function(integer) { return getNumeral(40) + getNumeral(integer-40) }
         },
         {
             matchRoman: function(integer) { if ( integer >= 30 && integer <= 39 ) return true }, 
-            getValue: function(integer) { return "XXX" + getNumeral(integer-30) }
+            getValue: function(integer) { return getNumeral(30) + getNumeral(integer-30) }
         },
         {
             matchRoman: function(integer) { if ( integer >= 20 && integer <= 29 ) return true }, 
-            getValue: function(integer) { return "XX" + getNumeral(integer-20) }
+            getValue: function(integer) { return getNumeral(20) + getNumeral(integer-20) }
         },
         {
             matchRoman: function(integer) { if ( integer >= 10 && integer <= 19 ) return true }, 
